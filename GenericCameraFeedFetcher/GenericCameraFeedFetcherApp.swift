67079d48a0531ae3a08f94c71480ea2cee4a6ad4
Shubham Kamdi <shubhamvkamdi@gmail.com>
2023-01-14 17:07:39 -0800

    Initial Commit

---
+++ GenericCameraFeedFetcher/GenericCameraFeedFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericCameraFeedFetcherApp.swift
+//  GenericCameraFeedFetcher
+//
+//  Created by Shubham Kamdi on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GenericCameraFeedFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
